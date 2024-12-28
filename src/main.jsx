ee94848b09caa9c00f0a416e54c0bb8d4e5296b5
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-28 16:40:09 +0545

    token add to navigate to login route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import UserProfileForm from "./components/UserAuth/UserProfileForm.jsx"; // The toggle component
 import App from "./App.jsx";
 
+const token = localStorage.getItem("token");
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
@@ -12,7 +13,10 @@ createRoot(document.getElementById("root")).render(
         <Routes>
           <Route path="/login" element={<UserProfileForm />} />
           <Route path="/register" element={<UserProfileForm />} />
-          <Route path="/" element={<App />} />
+          <Route
+            path="/"
+            element={token ? <App /> : <Navigate to="/login" />}
+          />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
