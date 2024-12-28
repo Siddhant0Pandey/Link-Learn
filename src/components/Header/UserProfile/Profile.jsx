b9360265ec353bd861f6057567a58370b98bce32
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-28 16:37:53 +0545

    logout link added

--- src/components/Header/UserProfile/Profile.jsx
+++ src/components/Header/UserProfile/Profile.jsx
@@ -1,17 +1,54 @@
+import { useEffect, useState } from "react";
 import { IoPersonCircleOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import { IoIosLogOut } from "react-icons/io";
 
 function Profile() {
+  const [username, setUserName] = useState("");
+  const [logout, setLogout] = useState(false);
+
+  useEffect(() => {
+    const storedUsername = localStorage.getItem("name");
+    if (storedUsername) {
+      setUserName(storedUsername);
+    }
+  }, []);
+
+  function toggleLogout() {
+    console.log("clicked");
+    setLogout(!logout);
+  }
+
   return (
-    <div className="profile_box">
-      <h5 className="profile_text">Name</h5>
-      <IoPersonCircleOutline
-        style={{
-          width: "30px",
-          height: "30px",
-          color: "#009963",
-        }}
-      />
-    </div>
+    <>
+      <div className="profile_box" onClick={toggleLogout}>
+        <h5 className="profile_text">{username}</h5>
+        <IoPersonCircleOutline
+          style={{
+            width: "30px",
+            height: "30px",
+            color: "#009963",
+          }}
+        />
+      </div>
+      {logout && (
+        <div className="logout_box">
+          <span>
+            <IoIosLogOut
+              style={{
+                width: "20px",
+                height: "20px",
+                paddingTop: "3px",
+              }}
+              className="logout_icon"
+            />
+          </span>
+          <Link to="/login" className="logout_text">
+            LogOut
+          </Link>
+        </div>
+      )}
+    </>
   );
 }
 
