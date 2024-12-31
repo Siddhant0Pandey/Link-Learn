5319ef806bc2353b48a3a2dfe3231d0f4e21cb81
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-31 20:58:26 +0545

    route added for tododlist

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,20 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import UserProfileForm from "./components/UserAuth/UserProfileForm.jsx"; // The toggle component
 import App from "./App.jsx";
+import TodoSection from "./components/TodoList/TodoSection.jsx";
 
 const token = localStorage.getItem("token");
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<UserProfileForm />} />
-          <Route path="/register" element={<UserProfileForm />} />
-          <Route
-            path="/"
-            element={token ? <App /> : <Navigate to="/login" />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-  </StrictMode>
+  <ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<UserProfileForm />} />
+        <Route path="/register" element={<UserProfileForm />} />
+        <Route path="/" element={token ? <App /> : <Navigate to="/login" />} />
+        <Route path="/tasks" element={<TodoSection />} />
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
 );
